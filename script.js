420726fcc2da74d413dc61d9b91ec0a36b397e6f
Charlotte Beeney <32536888+missbeeney@users.noreply.github.com>
2025-05-07 23:38:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     navMenu.classList.toggle("active");
   });
 });
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+  document.body.classList.toggle("menu-open");
+});
